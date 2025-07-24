e75b6224a8323811e576a703fb91ccd06684754b
Martino Facchin <m.facchin@arduino.cc>
2025-07-24 15:52:37 +0200

    unoq: also export matrixPlay API

--- loader/llext_exports.c
+++ loader/llext_exports.c
@@ -244,6 +244,7 @@ FORCE_EXPORT_SYM(__cxa_pure_virtual);
 #if defined(CONFIG_BOARD_B_U585I_IOT02A)
 FORCE_EXPORT_SYM(matrixBegin);
 FORCE_EXPORT_SYM(matrixWrite);
+FORCE_EXPORT_SYM(matrixPlay);
 FORCE_EXPORT_SYM(matrixGrayscaleWrite);
 FORCE_EXPORT_SYM(matrixSetGrayscaleBits);
 #endif
