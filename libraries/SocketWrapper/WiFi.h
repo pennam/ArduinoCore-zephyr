e89b0487083cabf1019c50eedb873ac084a18728
pennam <m.pennasilico@arduino.cc>
2025-09-08 09:55:11 +0200

    temp:network interface non blocking

--- libraries/SocketWrapper/WiFi.h
+++ libraries/SocketWrapper/WiFi.h
@@ -37,7 +37,7 @@ public:
 
         NetworkInterface::begin(false, NET_EVENT_WIFI_MASK);
         if (blocking) {
-            net_mgmt_event_wait_on_iface(sta_iface, NET_EVENT_WIFI_CONNECT_RESULT, NULL, NULL, NULL, K_FOREVER);
+            //net_mgmt_event_wait_on_iface(sta_iface, NET_EVENT_WIFI_CONNECT_RESULT, NULL, NULL, NULL, K_FOREVER);
         }
 
         return status();
