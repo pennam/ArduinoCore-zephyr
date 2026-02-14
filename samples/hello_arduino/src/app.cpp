zephyrproject-rtos/gsoc-2022-arduino-core

samples: hello_arduino: Correct printk format specifier
Fixed to use %zd which corresponds to size_t.

Signed-off-by: TOKITA Hiroshi <tokita.hiroshi@gmail.com>

samples/hello_arduino/src/app.cpp
@@ -16,14 +16,14 @@ void loop() {
   size_t ret2;
   ret1 = Serial.print(c);
   ret2 = Serial.println("Hello, World!");
-  printk("Sizes: %d %d\n", ret1, ret2);
+  printk("Sizes: %zd %zd\n", ret1, ret2);
   Serial.println();
   ret1 = Serial.print("My letter is: ");
   ret2 = Serial.println(c);
-  printk("Sizes: %d %d\n", ret1, ret2);
+  printk("Sizes: %zd %zd\n", ret1, ret2);
   Serial.println();
   char myString[] = "Will it print?";
   ret1 = Serial.println(myString);
-  printk("Size: %d \n\n\n", ret1);
+  printk("Size: %zd \n\n\n", ret1);
   delay(1000); // 1 second delay
 }

pennam/ArduinoCore-zephyr

samples/hello_arduino/src/app.cpp
@@ -16,14 +16,14 @@ void loop() {
   size_t ret2;
   ret1 = Serial.print(c);
   ret2 = Serial.println("Hello, World!");
-  printk("Sizes: %d %d\n", ret1, ret2);
+  printk("Sizes: %zu %zu\n", ret1, ret2);
   Serial.println();
   ret1 = Serial.print("My letter is: ");
   ret2 = Serial.println(c);
-  printk("Sizes: %d %d\n", ret1, ret2);
+  printk("Sizes: %zu %zu\n", ret1, ret2);
   Serial.println();
   char myString[] = "Will it print?";
   ret1 = Serial.println(myString);
-  printk("Size: %d \n\n\n", ret1);
+  printk("Size: %zu \n\n\n", ret1);
   delay(1000); // 1 second delay
 }