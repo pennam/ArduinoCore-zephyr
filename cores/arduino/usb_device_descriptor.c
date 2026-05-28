bfd9ed65ceeeafb91279ab1de7c72dfe5f6a0a40
Martino Facchin <m.facchin@arduino.cc>
2026-05-28 13:02:19 +0200

    usb: allow class reordering

--- cores/arduino/usb_device_descriptor.c
+++ cores/arduino/usb_device_descriptor.c
@@ -12,12 +12,18 @@
 
 #include <zephyr/logging/log.h>
 #include <zephyr/app_version.h>
+#if __has_include(<variant.h>)
+#include <variant.h>
+#endif
 
 #ifdef CONFIG_USB_DEVICE_STACK_NEXT
 
 /* By default, do not register the USB DFU class DFU mode instance. */
 static const char *const blocklist[] = {
 	"dfu_dfu",
+#if defined(LOADER_PROVIDES_EXTRA_USB_CLASSES)
+	"cdc_acm_0",
+#endif
 	NULL,
 };
 
@@ -74,6 +80,21 @@ static void sample_fix_code_triple(struct usbd_context *uds_ctx, const enum usbd
 	}
 }
 
+static int register_cdc_acm_0(struct usbd_context *const uds_ctx,
+			      const enum usbd_speed speed)
+{
+	struct usbd_config_node *cfg_nd;
+	int err;
+
+	err = usbd_register_class(&usbd, "cdc_acm_0", speed, 1);
+	if (err) {
+		return err;
+	}
+
+	return usbd_device_set_code_triple(uds_ctx, speed,
+					   USB_BCC_MISCELLANEOUS, 0x02, 0x01);
+}
+
 struct usbd_context *usbd_setup_device(usbd_msg_cb_t msg_cb) {
 	int err;
 
@@ -110,6 +131,10 @@ struct usbd_context *usbd_setup_device(usbd_msg_cb_t msg_cb) {
 			return NULL;
 		}
 
+		#if defined(LOADER_PROVIDES_EXTRA_USB_CLASSES)
+		register_cdc_acm_0(&usbd, USBD_SPEED_HS);
+		#endif
+
 		sample_fix_code_triple(&usbd, USBD_SPEED_HS);
 	}
 
@@ -126,6 +151,10 @@ struct usbd_context *usbd_setup_device(usbd_msg_cb_t msg_cb) {
 		return NULL;
 	}
 	/* doc functions register end */
+
+	#if defined(LOADER_PROVIDES_EXTRA_USB_CLASSES)
+	register_cdc_acm_0(&usbd, USBD_SPEED_HS);
+	#endif
 
 	sample_fix_code_triple(&usbd, USBD_SPEED_FS);
 
